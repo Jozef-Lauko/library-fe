e1674be51bf141283f3ff2a16287bdb35c4e0654
Jozef-Lauko <laukojozef1@gmail.com>
2023-02-09 19:57:55 +0100

    Lauko WeTe3 1st commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,12 +16,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'library-fe'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('library-fe');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,26 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
-// export class AppComponent {
-//   form = new FormGroup({
-//     hodnota: new FormControl()
-//   })
-//
-// }
-
-
 export class AppComponent {
   decimal = 0;
-  binary = '';
+  binary = [false, false, false, false, false, false, false, false];
 
-  calculate(dec: string): void {
-    let decimal = parseInt(dec)
-    if(decimal > 127){
-      decimal = 127
-    }
-
-    this.decimal = decimal;
-    let binary = "";
-
-    // iterate through the number till it becomes 0.
-    while (decimal > 0) {
-      // if modulo of number with 2 is ‘1’, append 1 in front of binary string. Otherwise append 0.
-      if (decimal % 2 == 1) {
-        binary = "1" + binary;
-      } else {
-        binary = "0" + binary;
-      }
-      // divide number by 2.
+  updateBinary() {
+    let decimal = this.decimal;
+    for (let i = 0; i <= 7; i++) {
+      this.binary[i] = decimal % 2 === 1;
       decimal = Math.floor(decimal / 2);
     }
+  }
 
-    this.binary = binary
+  updateDecimal() {
+    this.decimal = 0;
+    for (let i = 0; i <= 7; i++) {
+      this.decimal += this.binary[i] ? Math.pow(2, i) : 0;
+    }
   }
 }
