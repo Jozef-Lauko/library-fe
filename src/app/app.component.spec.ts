67aea4ee2ed4dd68ab0f4e878b09e90ce1209aa3
Lalik Marek <94965280+LalikMarek@users.noreply.github.com>
2023-02-10 07:52:50 +0100

    update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'library-fe'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('library-fe');
-  });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('library-fe app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('test app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'library-fe';
+  decimal = 0;
+  binary = [false, false, false, false, false, false, false, false];
+
+  updateBinary(dec: any) {
+    let decimal = parseInt(dec);
+    for (let i = 0; i <= 7; i++) {
+      this.binary[i] = decimal % 2 === 1;
+      decimal = Math.floor(decimal / 2);
+    }
+    this.decimal = parseInt(dec);
+  }
+
+  updateDecimal(num: number) {
+    this.decimal = 0;
+    this.binary[num] = !this.binary[num];
+
+    for (let i = 0; i <= 7; i++) {
+      this.decimal += this.binary[i] ? Math.pow(2, i) : 0;
+    }
+  }
 }
