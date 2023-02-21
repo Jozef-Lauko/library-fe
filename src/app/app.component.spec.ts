41ab29fe07a6508f29c5b80696cde1a7e68e7aba
Lalik Marek <94965280+LalikMarek@users.noreply.github.com>
2023-02-21 17:33:02 +0100

    Angular Projekt

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'library-fe'`, () => {
+  it(`should have as title 'cv2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('library-fe');
+    expect(app.title).toEqual('cv2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('library-fe app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('cv2 app is running!');
   });
 });
