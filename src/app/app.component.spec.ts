65e13c3d675afa85f1c0960fefe34486f63d491a
Lalik Marek <94965280+LalikMarek@users.noreply.github.com>
2023-02-23 15:45:14 +0100

    spec update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'cv2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cv2');
+    expect(app.title).toEqual('LibraryFe');
   });
 
   it('should render title', () => {
