4dd1b4034d6b81b8f78fc4fa63abc68d1eb14a86
Jozef-Lauko <laukojozef1@gmail.com>
2023-02-23 21:28:34 +0100

    BookComponent - Lauko - uloha3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { UserComponent } from './user/user.component';
+import {BookComponent} from "./book/book.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
